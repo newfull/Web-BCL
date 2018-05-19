f34daed4b2278137a0309ee0954e6860d5a0e787
Cong Nguyen <obmega3@gmail.com>
2018-05-19 13:09:08 +0700

    Update index slide

--- trang-chu.php
+++ trang-chu.php
@@ -3,69 +3,46 @@
 </head>
 <?php require_once("header.php");?>
 
-	<section id="slider">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-12">
-					<div id="slider-carousel" class="carousel slide" data-ride="carousel">
-						<ol class="carousel-indicators">
-							<li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
-							<li data-target="#slider-carousel" data-slide-to="1"></li>
-							<li data-target="#slider-carousel" data-slide-to="2"></li>
-						</ol>
 
-						<div class="carousel-inner">
-							<div class="item active">
-								<div class="col-sm-6">
-									<h1><span>E</span>-SHOPPER</h1>
-									<h2>Free E-Commerce Template</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-									<button type="button" class="btn btn-default get">Get it now</button>
-								</div>
-								<div class="col-sm-6">
-									<img src="images/home/girl1.jpg" class="girl img-responsive" alt="" />
-									<img src="images/home/pricing.png"  class="pricing" alt="" />
-								</div>
-							</div>
-							<div class="item">
-								<div class="col-sm-6">
-									<h1><span>E</span>-SHOPPER</h1>
-									<h2>100% Responsive Design</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-									<button type="button" class="btn btn-default get">Get it now</button>
-								</div>
-								<div class="col-sm-6">
-									<img src="images/home/girl2.jpg" class="girl img-responsive" alt="" />
-									<img src="images/home/pricing.png"  class="pricing" alt="" />
-								</div>
-							</div>
+<div id="slider-carousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#slider-carousel" data-slide-to="1"></li>
+    <li data-target="#slider-carousel" data-slide-to="2"></li>
+    <li data-target="#slider-carousel" data-slide-to="3"></li>
+    <li data-target="#slider-carousel" data-slide-to="4"></li>
+  </ol>
 
-							<div class="item">
-								<div class="col-sm-6">
-									<h1><span>E</span>-SHOPPER</h1>
-									<h2>Free Ecommerce Template</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-									<button type="button" class="btn btn-default get">Get it now</button>
-								</div>
-								<div class="col-sm-6">
-									<img src="images/home/girl3.jpg" class="girl img-responsive" alt="" />
-									<img src="images/home/pricing.png" class="pricing" alt="" />
-								</div>
-							</div>
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/promotions/index1.png" class="img-responsive img-fluid" alt="" />
+    </div>
 
-						</div>
+    <div class="item">
+      <img src="images/promotions/index2.png" class="img-responsive img-fluid" alt="" />
+    </div>
 
-						<a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
-							<i class="fa fa-angle-left"></i>
-						</a>
-						<a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
-							<i class="fa fa-angle-right"></i>
-						</a>
-					</div>
+    <div class="item">
+      <img src="images/promotions/index3.png" class="img-responsive img-fluid" alt="" />
+    </div>
 
-				</div>
-			</div>
-		</div>
-	</section>
+    <div class="item">
+      <img src="images/promotions/index4.png" class="img-responsive img-fluid" alt="" />
+    </div>
+
+    <div class="item">
+      <img src="images/promotions/index5.png" class="img-responsive img-fluid" alt="" />
+    </div>
+
+  </div>
+
+</div>
+
+  <a href="#slider-carousel" id="slider-carousel-prev" class="left control-carousel hidden-xs" data-slide="prev">
+    <i class="fa fa-angle-left"></i>
+  </a>
+  <a href="#slider-carousel" id="slider-carousel-next" class="right control-carousel hidden-xs" data-slide="next">
+    <i class="fa fa-angle-right"></i>
+  </a>
 
 	<?php require_once("footer.php");?>
